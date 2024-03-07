c6aa297db4f4c24d3879f252860c1977845b8112
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-03-07 17:00:12 +0800

    add

--- src/Customer.java
+++ src/Customer.java
@@ -20,4 +20,174 @@ public class Customer {
         public String email;
         public String ourReference;
         public String freeField6;
+
+        public String getCustomerNumber() {
+                return customerNumber;
+        }
+
+        public void setCustomerNumber(String customerNumber) {
+                this.customerNumber = customerNumber;
+        }
+
+        public String getCustomerName() {
+                return customerName;
+        }
+
+        public void setCustomerName(String customerName) {
+                this.customerName = customerName;
+        }
+
+        public String getCustomerType() {
+                return customerType;
+        }
+
+        public void setCustomerType(String customerType) {
+                this.customerType = customerType;
+        }
+
+        public String getUserName() {
+                return userName;
+        }
+
+        public void setUserName(String userName) {
+                this.userName = userName;
+        }
+
+        public Object getCompany() {
+                return company;
+        }
+
+        public void setCompany(Object company) {
+                this.company = company;
+        }
+
+        public Object getAddressNumber() {
+                return addressNumber;
+        }
+
+        public void setAddressNumber(Object addressNumber) {
+                this.addressNumber = addressNumber;
+        }
+
+        public String getAddress1() {
+                return address1;
+        }
+
+        public void setAddress1(String address1) {
+                this.address1 = address1;
+        }
+
+        public String getAddress2() {
+                return address2;
+        }
+
+        public void setAddress2(String address2) {
+                this.address2 = address2;
+        }
+
+        public String getAddress3() {
+                return address3;
+        }
+
+        public void setAddress3(String address3) {
+                this.address3 = address3;
+        }
+
+        public String getAddress4() {
+                return address4;
+        }
+
+        public void setAddress4(String address4) {
+                this.address4 = address4;
+        }
+
+        public String getPhone1() {
+                return phone1;
+        }
+
+        public void setPhone1(String phone1) {
+                this.phone1 = phone1;
+        }
+
+        public String getPhone2() {
+                return phone2;
+        }
+
+        public void setPhone2(String phone2) {
+                this.phone2 = phone2;
+        }
+
+        public String getIsVatExcempt() {
+                return isVatExcempt;
+        }
+
+        public void setIsVatExcempt(String isVatExcempt) {
+                this.isVatExcempt = isVatExcempt;
+        }
+
+        public String getPlace() {
+                return place;
+        }
+
+        public void setPlace(String place) {
+                this.place = place;
+        }
+
+        public String getCity() {
+                return city;
+        }
+
+        public void setCity(String city) {
+                this.city = city;
+        }
+
+        public String getFacsimileNumber() {
+                return facsimileNumber;
+        }
+
+        public void setFacsimileNumber(String facsimileNumber) {
+                this.facsimileNumber = facsimileNumber;
+        }
+
+        public String getReference1() {
+                return reference1;
+        }
+
+        public void setReference1(String reference1) {
+                this.reference1 = reference1;
+        }
+
+        public String getReference2() {
+                return reference2;
+        }
+
+        public void setReference2(String reference2) {
+                this.reference2 = reference2;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
+
+        public String getOurReference() {
+                return ourReference;
+        }
+
+        public void setOurReference(String ourReference) {
+                this.ourReference = ourReference;
+        }
+
+        public String getFreeField6() {
+                return freeField6;
+        }
+
+        public void setFreeField6(String freeField6) {
+                this.freeField6 = freeField6;
+        }
+
+
 }
--- src/JsonExample.java
+++ src/JsonExample.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.FileWriter;
@@ -5,24 +6,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonExample {
 
     public static void main(String[] args) {
         try {
-            String urlString = "http://192.168.1.51:88/Wilcon.TSSS.Tablet/Request.aspx?function=validatetimestamp&unix=1709608596";
+            String urlString = "http://192.168.1.62:88/Wilcon.TSSS.Tablet/Request.aspx?function=custsearchbyname&name=ortega";
 
             // Read data from the URL
             String jsonData = readJsonFromUrl(urlString);
 
             // Parse the JSON data
-            TimeStampObject timeStampObject = getTimeStampObjectFromJson(jsonData);
+            List<Customer> customerDataList = getCustomerList(jsonData);
 
             // Access the data as Java objects
-            System.out.println("Valid: " + timeStampObject.isValid());
+            for (Customer customer : customerDataList) {
+                // Write JSON data to a file for each customer
+                writeJsonToFile(customer);
+            }
 
-            // Write JSON data to a file
-            writeJsonToFile(new JSONObject(jsonData));
         } catch (IOException e) {
             e.printStackTrace(); // You might want to log the exception or provide a user-friendly message.
         }
@@ -37,20 +41,51 @@ public class JsonExample {
         }
     }
 
-    private static TimeStampObject getTimeStampObjectFromJson(String jsonData) {
-        TimeStampObject timeStampObject = new TimeStampObject();
+    private static List<Customer> getCustomerList(String jsonData) {
+        List<Customer> customerList = new ArrayList<>();
+        // Parse the JSON array
+        JSONArray jsonArray = new JSONArray(jsonData);
 
-        JSONObject jsonObject = new JSONObject(jsonData);
+        // Iterate through each object in the array
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            Customer customer = extractCustomerFromJson(jsonObject);
+            customerList.add(customer);
+        }
 
-        // Extract data from the JSON and set it in the Java object
-        timeStampObject.setValid(jsonObject.getString("valid"));
-
-        return timeStampObject;
+        return customerList;
     }
 
-    private static void writeJsonToFile(JSONObject jsonObject) throws IOException {
+    private static Customer extractCustomerFromJson(JSONObject jsonObject) {
+        Customer customer = new Customer();
+        customer.setCustomerNumber(jsonObject.getString("CustomerNumber"));
+        customer.setCustomerName(jsonObject.getString("CustomerName"));
+        customer.setCustomerType(jsonObject.getString("CustomerType"));
+        customer.setUserName(jsonObject.getString("UserName"));
+        customer.setCompany(jsonObject.optString("Company", ""));
+        customer.setAddress1(jsonObject.getString("Address1"));
+        customer.setAddress2(jsonObject.getString("Address2"));
+        customer.setAddress3(jsonObject.getString("Address3"));
+        customer.setAddress4(jsonObject.getString("Address4"));
+        customer.setPhone1(jsonObject.getString("Phone1"));
+        customer.setPhone2(jsonObject.getString("Phone2"));
+        customer.setIsVatExcempt(jsonObject.getString("IsVatExcempt"));
+        customer.setPlace(jsonObject.getString("Place"));
+        customer.setCity(jsonObject.getString("City"));
+        customer.setFacsimileNumber(jsonObject.getString("FacsimileNumber"));
+        customer.setReference1(jsonObject.getString("Reference1"));
+        customer.setReference2(jsonObject.getString("Reference2"));
+        customer.setEmail(jsonObject.getString("Email"));
+        customer.setOurReference(jsonObject.getString("OurReference"));
+        customer.setFreeField6(jsonObject.getString("FreeField6"));
+
+        return customer;
+    }
+
+    private static void writeJsonToFile(Customer customer) throws IOException {
         try (FileWriter fileWriter = new FileWriter("output.json")) {
-            fileWriter.write(jsonObject.toString(4)); // The '4' is the number of spaces for indentation
+            // Write JSON data to a file for the given customer
+            fileWriter.write(new JSONObject(customer).toString(4)); // The '4' is the number of spaces for indentation
         }
     }
 }
