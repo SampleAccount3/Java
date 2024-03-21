078733e2c8948e86147c50fe9a4e30f28667c123
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-03-21 08:32:47 +0800

    Done with the Basics of MultiThreading

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,25 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HEllo WOrld");
+
+        for (int i = 1; i <=5; i++ ){
+            MultiThreading multiThreading = new MultiThreading(i);
+            // implementing the Runnable has more advantage than extending the Thread
+            // call the Thread class then pass the ultiThreading class as a target
+            Thread myThread = new Thread(multiThreading);
+            // Starts the Thread
+            myThread.start();
+            try {
+                // w8s for the Thread to die then continue the next Thread
+                // Try Catch is Required
+                myThread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+
+
     }
 }
---
+++ src/MultiThreading.java
@@ -0,0 +1,21 @@
+public class MultiThreading implements Runnable {
+
+    int threadCount;
+    public MultiThreading (int threadCount){
+        this.threadCount = threadCount;
+    }
+
+
+    @Override
+    public synchronized void run() {
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i+ ": This is Thread " + threadCount);
+            try {
+                // delays for 1 sec
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
