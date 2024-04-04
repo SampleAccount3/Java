375efabe019df8f52fd71921f6ab24a6fb60ca9f
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-04-04 09:17:59 +0800

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /**
-         *  Assign the Enum in a variable
-         */
+//        Assign the Enum in a variable.
         SampleEnum round = SampleEnum.ROUND;
         SampleEnum square = SampleEnum.SQUARE;
-        /**
-         * .size() retrieves the size of the indicated enum
-         */
+//        .size() retrieves the size of the indicated enum.
         System.out.println(round.size());
         System.out.println(square.size());
 
         System.out.println(square.size() * 4_002.23123f);
     }
+
+    /**
+     * This is a generic method that swaps the positions of two elements in an array.
+     *
+     * @param <T> the type of the elements in the array
+     * @param array the array
+     * @param i the index of the first element
+     * @param j the index of the second element
+     */
+    public static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
 }
