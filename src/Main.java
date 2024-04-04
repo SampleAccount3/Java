4c49b9963997afbbc1820b5b619617989ed15322
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-04-04 08:29:23 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,18 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World");
+        VarLengthArgList("Melanio", "Flores");
+        // Output:
+        // Melanio
+        // Flores
 
-
+    }
+    // use String if you have multiple Arguments that has the same data type
+    // You can only declare it as a last argument
+    public static void VarLengthArgList(String... name){
+        for (String names: name){
+            System.out.println(names);
+        }
     }
 
 
