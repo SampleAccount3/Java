3c05e25b9c5783242242e0f67a6a482f94034fa3
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-04-04 08:35:05 +0800

    Enums

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        VarLengthArgList("Melanio", "Flores");
-        // Output:
-        // Melanio
-        // Flores
+    public static void main(String[] args) {
 
     }
-    // use String if you have multiple Arguments that has the same data type
-    // You can only declare it as a last argument
-    public static void VarLengthArgList(String... name){
-        for (String names: name){
-            System.out.println(names);
-        }
-    }
-
-
 }
